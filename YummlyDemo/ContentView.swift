e5f43f681a6d096bdd6f886b52c6ee6c99a6e3ac
prvnngrj <praveennagaraj2000@gmail.com>
2023-11-06 09:23:00 -0800

    Initial Commit

---
+++ YummlyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YummlyDemo
+//
+//  Created by Praveen Nagaraj on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YummlyDemo/YummlyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummlyDemoApp.swift
+//  YummlyDemo
+//
+//  Created by Praveen Nagaraj on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct YummlyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
